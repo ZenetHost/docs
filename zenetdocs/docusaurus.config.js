132ff7ac34557cb9b352603bc08ffdf58c25cb98
Peter <p33tcz@gmail.com>
2021-11-19 22:57:46 +0100

    edit fix

--- zenetdocs/docusaurus.config.js
+++ zenetdocs/docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/ZenetHost/docs/tree/main/zenetdocs/docs',
+          editUrl: 'https://github.com/ZenetHost/docs/tree/main/zenetdocs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/ZenetHost/docs/tree/main/zenetdocs/blog',
+            'https://github.com/ZenetHost/docs/tree/main/zenetdocs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
